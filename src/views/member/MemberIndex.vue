b509590a24646c7d7d117943d6ccc2e496e35d14
sfrancois99 <sebastien_f99@yahoo.com>
2019-03-26 21:18:31 +0100

    member index (part1) ok !

--- src/views/member/MemberIndex.vue
+++ src/views/member/MemberIndex.vue
@@ -1,15 +1,49 @@
 <template>
 	<div>
-		<h1>Member index</h1>
+	<h1>
+		Member Index
+	</h1>
+	<v-data-table :headers="headers" :items="membersList" class="elevation-1">
+    <template v-slot:items="props">
+      <td>{{ props.item.name }}</td>
+      <td class="text-xs-right">{{ props.item.calories }}</td>
+      <td class="text-xs-right">{{ props.item.fat }}</td>
+      <td class="text-xs-right">{{ props.item.carbs }}</td>
+      <td class="text-xs-right">{{ props.item.protein }}</td>
+      <td class="text-xs-right">{{ props.item.iron }}</td>
+    </template>
+  </v-data-table>
 	</div>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			//
+import { mapState } from "vuex";
+
+  export default {
+		computed : {
+			...mapState([
+			  'membersList',
+			]),
+		},
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Nom',
+            align: 'left',
+            sortable: true,
+            value: 'name'
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' }
+        ],
+      }
+    },
+		mounted () {
+		  this.$store.dispatch('getAllMembers');
 		}
-	}
-}
+  }
 </script>
