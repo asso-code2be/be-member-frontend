ef4efa42fe5a8fbf2ddded94b1b0c4c495c82a39
Yann <lowscaa@gmail.com>
2019-03-26 20:08:18 +0100

    eslint add semi rule

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,51 @@
 <template>
-    <v-app>
-        <v-toolbar app>
-            <router-link :to="{ name: 'dashboard'}">
-                <v-toolbar-title class="headline text-uppercase">
-                    <span>Code2.BE </span>
-                    <span class="font-weight-light">Be member</span>
-                </v-toolbar-title>
-            </router-link>
-
-            <router-link :to="{ name: 'member.index'}">
-                <v-btn flat>Membres</v-btn>
-            </router-link>
-
-            <v-spacer></v-spacer>
-            
-            <v-btn flat href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-                <span class="mr-2">Déconnexion</span>
-            </v-btn>
+    <v-app id="inspire">
+        <v-navigation-drawer v-model="drawer" fixed app>
+            <v-list dense>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>home</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Home</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-list-tile @click="">
+                    <v-list-tile-action>
+                        <v-icon>contact_mail</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>Contact</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
         </v-toolbar>
-
         <v-content>
-            <HelloWorld/>
+            <login />
         </v-content>
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; 2017</span>
+        </v-footer>
     </v-app>
 </template>
 
 <script>
-    import HelloWorld from './components/HelloWorld'
+import Login from './views/Login';
 
-    export default {
-        name: `App`,
-        components: {
-            HelloWorld
-        },
-        data() {
-            return {
-                //
-            }
-        }
-    }
+export default {
+	components: {
+		Login
+	},
+
+	data: () => ({
+		drawer: null
+	}),
+	props: {
+		source: String,
+	}
+};
 </script>
