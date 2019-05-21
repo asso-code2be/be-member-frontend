3bff120517445bb667dd578f4e6f1be6a39d64f9
Yann Aubriot <lowscaa@gmail.com>
2019-05-21 09:48:15 +0200

    eslint

--- src/views/member/MemberIndex.vue
+++ src/views/member/MemberIndex.vue
@@ -1,43 +1,51 @@
 <template>
-  <div>
-    <h1>Member Index</h1>
-    <v-data-table :headers="headers" :items="membersList" class="elevation-1">
-      <template v-slot:items="props">
-        <td>{{ props.item.id }}</td>
-        <td class="text-xs-right">{{ props.item.lastname }}</td>
-        <td class="text-xs-right">{{ props.item.firstname }}</td>
-        <td class="text-xs-right">
-          <v-btn :to="`/members/${props.item.id}/edit`">Modifier</v-btn>
-        </td>
-      </template>
-    </v-data-table>
-  </div>
+    <div>
+        <h1>Member Index</h1>
+        <v-data-table :headers="headers" :items="membersList" class="elevation-1">
+            <template v-slot:items="props">
+                <td>{{ props.item.id }}</td>
+                <td class="text-xs-right">
+                    {{ props.item.lastname }}
+                </td>
+                <td class="text-xs-right">
+                    {{ props.item.firstname }}
+                </td>
+                <td class="text-xs-right">
+                    <v-btn :to="`/members/${props.item.id}/edit`">
+                        Modifier
+                    </v-btn>
+                </td>
+            </template>
+        </v-data-table>
+    </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
 export default {
-  computed: {
-    ...mapState(["membersList"])
-  },
-  data() {
-    return {
-      headers: [
-        {
-          text: "#",
-          align: "left",
-          sortable: true,
-          value: "id"
-        },
-        { text: "Nom", value: "lastname" },
-        { text: "Prénom", value: "firstname" },
-        { text: "", value: "actions" }
-      ]
-    };
-  },
-  mounted() {
-    //this.$store.dispatch("getAllMembers");
-  }
+	data() {
+		return {
+			headers: [
+				{
+					text: `#`,
+					align: `left`,
+					sortable: true,
+					value: `id`
+				},
+				{ text: `Nom`, value: `lastname` },
+				{ text: `Prénom`, value: `firstname` },
+				{ text: ``, value: `actions` }
+			]
+		};
+	},
+
+	computed: {
+		...mapState([`membersList`])
+	},
+
+	mounted() {
+		//this.$store.dispatch("getAllMembers");
+	}
 };
 </script>
