1130dabf94cde02ef3a9b453f25372770e16cbda
Christian Barras <tirabc@gmail.com>
2019-09-03 20:31:55 +0200

    feat: add the delete member modal in the edit view

--- src/views/member/MemberEdit.vue
+++ src/views/member/MemberEdit.vue
@@ -1,82 +1,110 @@
 <template>
-    <v-container>
-        <h1>&Eacute;dition d'un membre</h1>
+  <v-container>
+    <h1>&Eacute;dition d'un membre</h1>
 
-        <v-layout row wrap>
-            <v-flex md4 xs12>
-                <v-container grid-list-8>
-                    <v-text-field label="Nom" required v-model="member.firstname" />
-                </v-container>
-            </v-flex>
+    <v-layout row wrap>
+      <v-flex md4 xs12>
+        <v-container grid-list-8>
+          <v-text-field label="Nom" required v-model="member.firstname" />
+        </v-container>
+      </v-flex>
 
-            <v-flex md4 xs12>
-                <v-container grid-list-8>
-                    <v-text-field label="Prénom" required v-model="member.lastname" />
-                </v-container>
-            </v-flex>
+      <v-flex md4 xs12>
+        <v-container grid-list-8>
+          <v-text-field label="Prénom" required v-model="member.lastname" />
+        </v-container>
+      </v-flex>
 
-            <v-flex md4 xs12>
-                <v-container grid-list-8>
-                    <v-text-field label="E-mail" type="email" required v-model="member.email" />
-                </v-container>
-            </v-flex>
-        </v-layout>
+      <v-flex md4 xs12>
+        <v-container grid-list-8>
+          <v-text-field label="E-mail" type="email" required v-model="member.email" />
+        </v-container>
+      </v-flex>
+    </v-layout>
 
-        <v-btn @click="save">
-            Enregistrer
-        </v-btn>
-
-        <!-- todo: add delete button -->
-    </v-container>
+    <v-btn @click="save">Enregistrer</v-btn>
+    <v-btn @click="confirmDeleteMember">Supprimer</v-btn>
+    <modal-delete
+      :visible.sync="modalDeleteVisibility"
+      :width="450"
+      @canceled="modalDeleteVisibility=false"
+      @confirmed="deleteMember"
+      v-if="modalMemberToDelete"
+    >
+      <template
+        v-slot:content
+      >Êtes vous sur de vouloir supprimer {{ modalMemberToDelete.firstname }} {{ modalMemberToDelete.lastname }} ?</template>
+    </modal-delete>
+  </v-container>
 </template>
 
 <script>
-import store from '../../store/index';
-import cloneDeep from 'lodash/cloneDeep';
+import store from "../../store/index";
+import cloneDeep from "lodash/cloneDeep";
+import ModalDelete from "../../components/ModalDelete";
 
 export default {
-	name: `MemberEdit`,
+  name: `MemberEdit`,
 
-	props: {
-		id: {
-			type: Number,
-			default: null,
-		}
-	},
+  props: {
+    id: {
+      type: Number,
+      default: null
+    }
+  },
 
-	data() {
-		return {
-			member: cloneDeep(store.state.member),
-			saving: false,
-		};
-	},
+  components: {
+    ModalDelete
+  },
 
-	async beforeRouteEnter(to, from, next) {
-		try {
-			await store.getters.getMemberById(to.params.id);
-			next();
-		} catch (e) {
-			console.log(e);
-			console.log(`Erreur lors de la récupération du membre.`);
-		}
-	},
+  data() {
+    return {
+      member: cloneDeep(store.state.member),
+      saving: false,
+      modalDeleteVisibility: false,
+      modalMemberToDelete: null
+    };
+  },
 
-	methods: {
-		async save() {
-			this.saving = true;
-			try {
-				await store.dispatch(`updateMember`, this.member);
-			} catch (e) {
-				console.log(e);
-				console.log(`Erreur lors de la mise à jour du membre.`);
-			}
-			this.saving = false;
-			this.$router.push({name:`member.index`});
-		},
+  async beforeRouteEnter(to, from, next) {
+    try {
+      await store.getters.getMemberById(to.params.id);
+      next();
+    } catch (e) {
+      console.log(e);
+      console.log(`Erreur lors de la récupération du membre.`);
+    }
+  },
 
-		delete() {
-			// todo : delete this member
-		}
-	}
+  methods: {
+    async save() {
+      this.saving = true;
+      try {
+        await store.dispatch(`updateMember`, this.member);
+      } catch (e) {
+        console.log(e);
+        console.log(`Erreur lors de la mise à jour du membre.`);
+      }
+      this.saving = false;
+      this.$router.push({ name: `member.index` });
+    },
+
+    confirmDeleteMember(member) {
+      this.modalMemberToDelete = member;
+      this.modalDeleteVisibility = true;
+    },
+
+    async deleteMember() {
+      this.modalDeleteVisibility = true;
+      try {
+        await store.dispatch(`deleteMemberById`, this.member.id);
+      } catch (e) {
+        console.log(e);
+        console.log(`Erreur durant la suppression du membre.`);
+      }
+      this.deleting = false;
+      this.$router.push({ name: `member.index` });
+    }
+  }
 };
 </script>
